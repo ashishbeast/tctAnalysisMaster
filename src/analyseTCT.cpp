f4bd48130ea77769ffeb9fbdd090bc3942061eb9
ashishbeast <ash007.1417@gmail.com>
2022-12-16 17:20:03 +0100

    Added Example Filesgit add *

---
+++ src/analyseTCT.cpp
@@ -0,0 +1,28 @@
+#include "AnalyzeTCTData.h"
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if(argc < 2)
+    {
+      cout<<"[WARNING] : \tUsage: ./analyseTCT fileName.tct \n";
+      return -1;
+    }
+
+  //Read and Analyze the file
+  for(Int_t i = 1; i < argc; ++i)
+    {
+      AnalyzeTCTData tct(argv[i]);
+      tct.CorrectBaseline();
+      tct.CalcNoise();
+      tct.CalculateSignalProperties();
+      tct.SaveSignalShape();
+      tct.AnalysisAction();
+    }
+  return 0;
+}
